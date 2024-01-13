268267876df4ccb40646f112bdd5a97cbb7881bb
lurah11 <indrajid.lurah@gmail.com>
2024-01-14 06:41:31 +0700

    add example of .env files

--- dj_autosub/settings.py
+++ dj_autosub/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 import os
 
 # load environment variables 
-load_dotenv('../.env')
+load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
